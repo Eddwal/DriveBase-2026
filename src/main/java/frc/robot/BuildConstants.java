549f1ad86d22a89834b88bd47fe5e3f858c6aa9e
liamn <liam.b@skunkworks1983.com>
2025-10-30 18:56:01 -0700

    Builds and works, Starting vision implementation

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "advantage-kit-2025";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 137;
+  public static final String GIT_SHA = "3dd10dc99a9319a342e846124f2ac373fad3d737";
+  public static final String GIT_DATE = "2025-10-18 12:05:06 EDT";
+  public static final String GIT_BRANCH = "autosToImageDashboard";
+  public static final String BUILD_DATE = "2025-10-21 23:08:18 EDT";
+  public static final long BUILD_UNIX_TIME = 1761102498127L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants() {}
+}
